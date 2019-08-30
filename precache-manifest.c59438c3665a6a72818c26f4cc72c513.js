701026da6ce145caa6183e848054dcb7c2aca2db
Misacorp <misa.jokisalo@gmail.com>
2019-08-30 16:50:44 +0300

    Updates

---
+++ precache-manifest.c59438c3665a6a72818c26f4cc72c513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a877578fb3175e23b4625ac36090a8",
+    "url": "/game-pass-recommendations/index.html"
+  },
+  {
+    "revision": "dd47544b88449d272019",
+    "url": "/game-pass-recommendations/static/css/main.036f711b.chunk.css"
+  },
+  {
+    "revision": "f7f077e2cc513624776f",
+    "url": "/game-pass-recommendations/static/js/2.5686786a.chunk.js"
+  },
+  {
+    "revision": "dd47544b88449d272019",
+    "url": "/game-pass-recommendations/static/js/main.76cee8d7.chunk.js"
+  },
+  {
+    "revision": "4c80f3379cc9946a8c42",
+    "url": "/game-pass-recommendations/static/js/runtime~main.727707cb.js"
+  }
+]);
